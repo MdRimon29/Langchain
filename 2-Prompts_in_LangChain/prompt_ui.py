ed31f7fa364442b474f400c4c028d574483e5ade
Rimon <mdraisul.i.rimon@gmail.com>
2025-09-18 21:34:22 +0600

    basic skeleton for prompt in langchain

---
+++ 2-Prompts_in_LangChain/prompt_ui.py
@@ -0,0 +1,18 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+model=ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0,
+)
+
+st.header("Research Tool")
+
+user_input=st.text_input("Enter your prompt: ")
+
+if st.button("Summarize"):
+    result=model.invoke(user_input)
+    st.write(result.content)
