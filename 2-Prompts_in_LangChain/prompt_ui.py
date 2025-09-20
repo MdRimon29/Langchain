fc47d1129e6b922e31ef0a5a271faac41a1466fa
Rimon <mdraisul.i.rimon@gmail.com>
2025-09-20 15:05:10 +0600

    updated for prompt

--- 2-Prompts_in_LangChain/prompt_ui.py
+++ 2-Prompts_in_LangChain/prompt_ui.py
@@ -11,8 +11,15 @@ model=ChatGroq(
 
 st.header("Research Tool")
 
-user_input=st.text_input("Enter your prompt: ")
+# Asking prompt to user.
+# user_input=st.text_input("Enter your prompt: ")
+
+# Instead of prompt now we do it dynamically by asking select dropdown options.
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
 if st.button("Summarize"):
-    result=model.invoke(user_input)
-    st.write(result.content)
+    st.write("Hello")
