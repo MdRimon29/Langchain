90ef84aeabbbeb914880641a6ebdfb8e83a42072
Rimon <mdraisul.i.rimon@gmail.com>
2025-09-20 17:28:17 +0600

    add message file and fixing the type of messages

--- 2-Prompts_in_LangChain/chatbot.py
+++ 2-Prompts_in_LangChain/chatbot.py
@@ -1,4 +1,5 @@
 from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,15 +8,17 @@ model=ChatGroq(
     model="llama-3.1-8b-instant"
 )
 
-chat_history=[] # here we store the context
+chat_history=[
+    SystemMessage(content='You are a helpful AI assistant')
+] # here we store the context
 
 while True:
     prompt= input("User: ")
-    chat_history.append(prompt)
+    chat_history.append(HumanMessage(content=prompt))
     if prompt=="exit":
         break
     result=model.invoke(chat_history)
-    chat_history.append(result)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ",result.content)
 
 print(chat_history)
