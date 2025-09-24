4b5144d0e94b0a97aaeec9aa9dc1dd8398283fe6
Rimon <mdraisul.i.rimon@gmail.com>
2025-09-24 12:38:07 +0600

    basic pydantic demo

--- 3-Structured_Output_in_LangChain/03-pydantic_demo.py
+++ 3-Structured_Output_in_LangChain/03-pydantic_demo.py
@@ -1,10 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,Field
+from typing import Optional
 
 class Student(BaseModel):
-    name: str
+    name: str = "Rimon"
+    age: Optional[int] = None
+    email: EmailStr
+    cgpa: float =Field(gt=0,lt=4, default=5, description='A decimal value representing the cgpa of the student')
 
-new_student={'name':32} #this gives an error
+new_student={'age':'24', 'email':'abc@gmail.com'}
 
 student=Student(**new_student)
 
 print(student)
+
+student_dict = dict(student)
+
+print(student_dict['age'])
