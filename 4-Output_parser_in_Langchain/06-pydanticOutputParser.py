3171c14cefb562f049ca8e32862b1b330f6397ef
Rimon <mdraisul.i.rimon@gmail.com>
2025-09-27 13:48:49 +0600

    updated for pydantic output parser by which we can validate and enforce strict schema

---
+++ 4-Output_parser_in_Langchain/06-pydanticOutputParser.py
@@ -0,0 +1,30 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel,Field
+
+load_dotenv()
+
+model=ChatGroq(
+    model="llama-3.1-8b-instant"
+)
+
+class Person(BaseModel):
+    name: str=Field(description="Name of the person")
+    age: int=Field(gt=18, lt=60, description="Age of that person")
+    city: str = Field(description='Name of the city the person belongs to')
+
+parser=PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'Bangladeshi'})
+
+print(final_result)
